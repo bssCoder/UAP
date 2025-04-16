623638678389a4d42fac30966ecead0ce1b1fc92
Bhawani Shankar Sarswat <bshankarsarswat8239@gmail.com>
2025-04-17 01:16:36 +0530

    new web added

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://uap-pi.vercel.app"],
+    origin: ["http://localhost:3000", "https://uap-pi.vercel.app","https://uapages.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
